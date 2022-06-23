f6d3652049b820fee45b7b3e98d3a0e009b00ef0
rw-nipuni <nipuniudari@gmail.com>
2022-06-23 08:23:20 +0530

    Adding a new project

--- src/portfolio.js
+++ src/portfolio.js
@@ -18,7 +18,7 @@ const greeting = {
     " I'm all about combining visual design with product thinking to bring meaningful experiences alive who's passionate about creating digital experiences through problem-solving. I fell in love with coding, exploring, travelling, Photography,Painting,designing."
   ),
   resumeLink:
-  "https://drive.google.com/file/d/1iR4_Htg02mu0Xr2Na1TqKraRKH7ltnn_/view?usp=sharing", // Set to empty to hide the button
+  "https://drive.google.com/file/d/1iRBY31Nx2cccHF7BTYY9SlirOuSmOX0T/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
@@ -261,7 +261,7 @@ const workExperiences = {
 To know how to get github key look at readme.md */
 
 const openSource = {
-  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  showGithubProfile: "false", // Set true or false to show Contact profile using Github, defaults to true
   display: true // Set false to hide this section, defaults to true
 };
 
@@ -269,7 +269,7 @@ const openSource = {
 
 const bigProjects = {
   title: "My Projects",
-  subtitle: "There are many kind of projects I have done so far.",
+  subtitle: "There are many kind of projects that I have done so far.",
   projects: [
     {
       image: require("./assets/images/p1.png"),
@@ -278,7 +278,7 @@ const bigProjects = {
       
       footerLink: [
         {
-          name: "Visit Website",
+          name: "Visit project",
           url: "https://www.behance.net/gallery/138281981/Cooffe-table-book"
         },
         //  you can add extra buttons here.
@@ -290,7 +290,7 @@ const bigProjects = {
       projectDesc: "A good hotel website should have a clean design,be easy to navigate and include a lot of social proof. Have a look at this page . I have designed Website for cinnoman lodge hotel Habarana using Html, css, Js.It contains Facilities,Gallery,contacts,services, Feedbacks etc:It's easy to use and navigate between tabs.",
       footerLink: [
         {
-          name: "Visit Website",
+          name: "Visit project",
           url: "https://gdlmmuf5mkdbjxbzimtqhq-on.drv.tw/www.cinnomanhotel.com/Cinnoman%20Hotel%20Website/Main.html"
         },
         {
@@ -308,7 +308,15 @@ const bigProjects = {
         {
           name: "view Source code",
           url: "https://github.com/rw-nipuni"
-        }
+        },
+        {
+          name: "view UI design",
+          url: "https://www.figma.com/file/SeQDpKXI3DzPL6HvQUeYS3/FINAL-PROJECT-Xpress-E-TECH-APP-(AF%2F18%2F14804)?node-id=29%3A196"
+        },
+        {
+          name: "view Prototype",
+          url: "https://www.figma.com/proto/SeQDpKXI3DzPL6HvQUeYS3/FINAL-PROJECT-Xpress-E-TECH-APP-(AF%2F18%2F14804)?node-id=3%3A5&scaling=scale-down&page-id=0%3A1&starting-point-node-id=3%3A2&show-proto-sidebar=1"
+        },
       ]
     },{
       image: require("./assets/images/p4.png"),
@@ -335,19 +343,21 @@ const bigProjects = {
       ]
     },
     {
-      image: require("./assets/images/j6.png"),
-      projectName: "Mobile Application User Interface",
-      projectDesc: "The first impressions are 94% design related.I designed a Mobile App user interface using figma online tool.It contains Splash screens,Onboarding Screens,Login and profile screens,cart, Checkout screens and other E commerce screens.Accourding to the given feedbacks it is a user friendly UI and easy to use.",
+      image: require("./assets/images/kod.png"),
+      projectName: "company website User Interface revamping",
+      projectDesc: "The first impressions are 94% design related.I designed a website user interface using figma online tool.I got a new look for the website by Using different buttons, navigations, colors , pictures.Accourding to the given feedbacks it is a user friendly UI and easy to use.Rather than the old ui it's get a new cool look for the website homepage",
       footerLink: [
+
+        {
+          name: "view Template",
+          url: "https://www.figma.com/file/8U6oJJJfHYCHv8sONc8iLc/kodplex-company-website?node-id=0%3A1"
+        },
         
         {
           name: "view Prototype",
-          url: "https://www.figma.com/proto/SeQDpKXI3DzPL6HvQUeYS3/FINAL-PROJECT-Xpress-E-TECH-APP-(AF%2F18%2F14804)?node-id=3%3A5&scaling=scale-down&page-id=0%3A1&starting-point-node-id=3%3A2&show-proto-sidebar=1"
+          url: "https://www.figma.com/proto/8U6oJJJfHYCHv8sONc8iLc/kodplex-company-website?node-id=2%3A2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=2%3A2"
         },
-        {
-          name: "view Template",
-          url: "https://www.figma.com/file/SeQDpKXI3DzPL6HvQUeYS3/FINAL-PROJECT-Xpress-E-TECH-APP-(AF%2F18%2F14804)?node-id=29%3A196"
-        },
+        
 
       ]
     },
@@ -358,7 +368,7 @@ const bigProjects = {
       
       footerLink: [
         {
-          name: "Visit Website",
+          name: "View projects",
           url: "https://www.behance.net/nipuniudari1"
         },
         //  you can add extra buttons here.
@@ -371,8 +381,12 @@ const bigProjects = {
       
       footerLink: [
         {
-          name: "Visit Website",
+          name: "Visit source code",
           url: "https://github.com/nipuni-udari/portfolio-nipuniUdari"
+        },
+        {
+          name: "Visit website",
+          url: "https://www.nipuniudari.tech/"
         },
         //  you can add extra buttons here.
       ]
